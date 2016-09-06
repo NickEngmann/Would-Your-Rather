e343329e29bf3d64db98ff4f48ca38c1b59e3c7f
Nick Engmann <nickengmann_2@yahoo.com>
2016-09-06 12:33:06 -0700

    V1 Complete
    Version 1 is complete and ready to ship

--- WYR/ViewController.swift
+++ WYR/ViewController.swift
@@ -13,37 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var TopQuestion: UILabel!
     @IBOutlet weak var BottomQuestion: UILabel!
     @IBOutlet weak var NextButton: UIButton!
-    var Questions = ["Have No Air Conditioning in the Middle of Summer", "Have No Heat in the Middle of Winter", "Lose Your Dominant Arm and Get an Extra Leg", "Lose Your Dominant Leg and Get an Extra Arm", "Fight Yourself on Bathsalts", "Fight Two of Your Regular Selfs", "Die In a Strange Way", "Die In a Midly Heroic Way"]
-    //8
+    var Questions = ["Have No Air Conditioning in the Middle of Summer", "Have No Heat in the Middle of Winter", "Lose Your Dominant Arm and Get an Extra Leg", "Lose Your Dominant Leg and Get an Extra Arm", "Fight Yourself on Bathsalts", "Fight Two of Your Regular Selfs", "Die In a Strange Way", "Slowly Drag the Palm of Your Hand Across a Hot Stove","Quickly Swipe the Palm of Your Hand Across a Lightly Splintered Wooden Plank", "Drag Your EyeBall Across Any Surface Before You Touch It", "Have a Small Insect, that You Can Never Kill or Pinpoint, Crawling Under Your Scalp For the Rest of Your Life", "Always Have Sticky Hands", "Always have Greasy Hands", "Always Have To Drink Watered Down Soda", "Always Have to Drink Flat Diet Soda", "Have Your Parents Find Out Every Drug You Have Ever Done", "Crawl Everywhere for the Rest of Your Life", "Have Baby Sized Hands", "Only be able to Whisper for the Rest of Your Life", "Lose All Your Fingers", "Lose all Your Toes"]
+    //21,
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        self.TopQuestion.text = Questions[Int(arc4random_uniform(8))]
-        self.BottomQuestion.text = Questions[Int(arc4random_uniform(8))]
+        self.TopQuestion.text = Questions[Int(arc4random_uniform(21))]
+        self.BottomQuestion.text = Questions[Int(arc4random_uniform(21))]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
-    func handleTap(sender: UITapGestureRecognizer) {
-        self.TopQuestion.text = Questions[Int(arc4random_uniform(8))]
-    }
     @IBAction func NextTapped(sender: UIButton) {
-        self.TopQuestion.text = Questions[Int(arc4random_uniform(8))]
-        self.BottomQuestion.text = Questions[Int(arc4random_uniform(8))]
+        self.TopQuestion.text = Questions[Int(arc4random_uniform(21))]
+        self.BottomQuestion.text = Questions[Int(arc4random_uniform(21))]
     }
     @IBAction func TopQuestionTapped(sender: UIButton) {
-        self.TopQuestion.text = Questions[Int(arc4random_uniform(8))]
-        self.BottomQuestion.text = Questions[Int(arc4random_uniform(8))]
+        self.TopQuestion.text = Questions[Int(arc4random_uniform(21))]
+        self.BottomQuestion.text = Questions[Int(arc4random_uniform(21))]
     }
     @IBAction func BottomQuestionTapped(sender: UIButton) {
-        self.TopQuestion.text = Questions[Int(arc4random_uniform(8))]
-        self.BottomQuestion.text = Questions[Int(arc4random_uniform(8))]
+        self.TopQuestion.text = Questions[Int(arc4random_uniform(21))]
+        self.BottomQuestion.text = Questions[Int(arc4random_uniform(21))]
     }
 }
 
